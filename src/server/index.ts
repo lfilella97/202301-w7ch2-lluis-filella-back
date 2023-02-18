ade75614061f76193ba991d01701618ca3b04dbf
lfilella97 <lfilella97@gmail.com>
2023-02-18 14:34:12 +0100

    Add robots controller to emit a response

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import startServer from "./startServer.js";
+import { getRobots } from "./controllers/robotsControllers.js";
 
 const app = express();
+const port = process.env.PORT ?? 4000;
+
 app.use(express.json());
 
-const port = process.env.PORT ?? 4000;
+app.get("/robots", getRobots);
 
 startServer(+port);
 
