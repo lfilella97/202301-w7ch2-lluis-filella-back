24ed81189e419f856047fbaf22bd423cc2751263
lfilella97 <lfilella97@gmail.com>
2023-02-19 17:51:43 +0100

    Add CORS options

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { getRobots } from "./controllers/robotsControllers.js";
 
 const app = express();
+const allowedOrigins = ["https://w6chwe-marcel-lluis.onrender.com"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
+app.use(cors(options));
 app.use(morgan("dev"));
 app.use(express.json());
 
