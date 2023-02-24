8e8af1587e46a2f0397f73980f0e1ff123de8224
lfilella97 <lfilella97@gmail.com>
2023-02-24 13:17:41 +0100

    Add compare method from bcrypt to validate user password

--- src/server/controllers/userControllers/loginUser/loginUser.ts
+++ src/server/controllers/userControllers/loginUser/loginUser.ts
@@ -1,9 +1,9 @@
-import "../../../../loadEnvoirements.js";
 import jwt from "jsonwebtoken";
 import { type NextFunction, type Request, type Response } from "express";
 import { type UserCredentials } from "../../../../types";
 import CustomError from "../../../../CustomError/CustomError.js";
 import User from "../../../../database/models/userSchema.js";
+import bcrypt from "bcryptjs";
 
 const loginUser = async (
   req: Request<
@@ -16,11 +16,23 @@ const loginUser = async (
 ) => {
   const { password, userName } = req.body;
 
-  const user = await User.findOne({ userName, password });
+  const user = await User.findOne({ userName });
 
   if (!user) {
     const customError = new CustomError(
-      "Incorrect credentials",
+      "Incorrect user",
+      401,
+      "Incorrect credentials"
+    );
+    next(customError);
+    return;
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    const customError = new CustomError(
+      "Incorrect password",
       401,
       "Incorrect credentials"
     );
